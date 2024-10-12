29d1742f528ef135146384c71c28651e1277dca4
Animesh239 <animeshbarik239@gmail.com>
2024-10-12 22:34:17 +0530

    updated base url

--- Gemini-Clone/src/context/Context.jsx
+++ Gemini-Clone/src/context/Context.jsx
@@ -1,10 +1,94 @@
-import {createContext, useState} from "react";
-import runChat from "../config/gemini";
+// import {createContext, useState} from "react";
+// import runChat from "../config/gemini";
+
+// export const Context = createContext();
+
+// const ContextProvider = (props) => {
+
+//     const [input, setInput] = useState("");
+//     const [recentPrompt, setRecentPrompt] = useState("");
+//     const [prevPrompts, setPrevPrompts] = useState([]);
+//     const [showResult, setShowResult] = useState(false);
+//     const [loading, setLoading] = useState(false);
+//     const [resultData, setResultData] = useState("");
+
+//     const delayPara = (index, nextWord) => {
+//         setTimeout(function () {
+//             setResultData(prev => prev + nextWord)
+//         }, 75 * index);
+//     }
+
+//     const newChat = () => {
+//         setLoading(false);
+//         setShowResult(false);
+//     }
+
+//     const onSent = async (prompt) => {
+
+
+//         setResultData("");
+//         setLoading(true);
+//         setShowResult(true);
+//         let response;
+//         if (prompt !== undefined) {
+//             response = await runChat(prompt);
+//             setRecentPrompt(prompt);
+//         } else {
+//             setPrevPrompts(prev => [...prev, input]);
+//             setRecentPrompt(input);
+//             response = await runChat(input);
+//         }
+
+//         let responseArray = response.split("**");
+//         let newResponse = "";
+//         for (let i = 0; i < responseArray.length; i++) {
+//             if (i === 0 || i % 2 !== 1) {
+//                 newResponse += responseArray[i];
+//             } else {
+//                 newResponse += "<b>" + responseArray[i] + "</b>"
+//             }
+
+//         }
+//         let newResponse2 = newResponse.split("*").join("</br>");
+//         let newResponseArray = newResponse2.split(" ");
+//         for (let i = 0; i < newResponseArray.length; i++) {
+//             const nextWord = newResponseArray[i];
+//             delayPara(i, nextWord + " ");
+//         }
+//         setLoading(false);
+//         setInput("");
+//     }
+
+//     const contextValue = {
+//         prevPrompts,
+//         setPrevPrompts,
+//         onSent,
+//         recentPrompt,
+//         setRecentPrompt,
+//         showResult,
+//         loading,
+//         resultData,
+//         input,
+//         setInput,
+//         newChat
+//     }
+//     return (
+//         <Context.Provider value={contextValue}>
+//             {props.children}
+//         </Context.Provider>
+//     )
+// }
+
+// export default ContextProvider;
+
+
+
+import { createContext, useState } from "react";
+
 
 export const Context = createContext();
 
 const ContextProvider = (props) => {
-
     const [input, setInput] = useState("");
     const [recentPrompt, setRecentPrompt] = useState("");
     const [prevPrompts, setPrevPrompts] = useState([]);
@@ -23,34 +107,41 @@ const ContextProvider = (props) => {
         setShowResult(false);
     }
 
+    const baseURL=import.meta.env.VITE_BASE_URL;
+
     const onSent = async (prompt) => {
-
-
         setResultData("");
         setLoading(true);
         setShowResult(true);
         let response;
-        if (prompt !== undefined) {
-            response = await runChat(prompt);
-            setRecentPrompt(prompt);
-        } else {
-            setPrevPrompts(prev => [...prev, input]);
-            setRecentPrompt(input);
-            response = await runChat(input);
-        }
+        const userPrompt = prompt !== undefined ? prompt : input;
 
-        let responseArray = response.split("**");
-        let newResponse = "";
-        for (let i = 0; i < responseArray.length; i++) {
-            if (i === 0 || i % 2 !== 1) {
-                newResponse += responseArray[i];
-            } else {
-                newResponse += "<b>" + responseArray[i] + "</b>"
+        try {
+            const res = await fetch(`${baseURL}/predict`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ user: userPrompt })
+            });
+
+            if (!res.ok) {
+                throw new Error('Network response was not ok');
             }
 
+            const data = await res.json();
+            response = data.agent;
+        } catch (error) {
+            console.error('Error:', error);
+            response = "Sorry, there was an error processing your request.";
         }
-        let newResponse2 = newResponse.split("*").join("</br>");
-        let newResponseArray = newResponse2.split(" ");
+
+        setRecentPrompt(userPrompt);
+        if (prompt === undefined) {
+            setPrevPrompts(prev => [...prev, userPrompt]);
+        }
+
+        let newResponseArray = response.split(" ");
         for (let i = 0; i < newResponseArray.length; i++) {
             const nextWord = newResponseArray[i];
             delayPara(i, nextWord + " ");
@@ -72,6 +163,7 @@ const ContextProvider = (props) => {
         setInput,
         newChat
     }
+
     return (
         <Context.Provider value={contextValue}>
             {props.children}
